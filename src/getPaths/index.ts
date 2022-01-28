1a8c1bf43defe35f6cca6b9399eda7c1c8461963
Julien Lavocat <julien.lavocat@gmail.com>
2022-01-28 08:10:14 +0100

    move to use shapes

--- src/getPaths/index.ts
+++ src/getPaths/index.ts
@@ -12,68 +12,27 @@ export interface SequencedStop {
 
 export type Path = [string, number][];
 
-export function getPaths(timetable: StopTimeTable): {
-	paths: { paths?: string[][]; mergeAt: string; continueTo: string }[];
-	mainPath: SequencedStop[];
-} {
-	const paths = getPathsFromTrips(timetable.services);
-	const trips = [paths.mainPath, ...paths.trips];
+export interface Shape {
+	entries: string[];
+	exits: string[];
+	paths: string[][];
+}
 
-	const mainPathIds = paths.mainPath.map((e) => e.stopId);
+export function getPaths(timetable: StopTimeTable): Shape[] {
+	const { mainPath, trips } = getPathsFromTrips(timetable.services);
+	const mainPathIds = mainPath.map((e) => e.stopId);
 
-	if (trips.length < 2) {
-		// It's a simple line, with no other routes associated
-		return {
-			paths: [],
-			mainPath: paths.mainPath,
-		};
-	}
+	if (!trips)
+		// Simple line, no branches, loops, etc.
+		return [
+			{
+				entries: [mainPath[0].stopId],
+				exits: [mainPath[mainPath.length - 1].stopId],
+				paths: [mainPath.map((e) => e.stopId)],
+			},
+		];
 
-	const extractPathFromSplit = (array: string[], common: string) => {
-		const mainPathId = mainPathIds[mainPathIds.indexOf(common) - 1];
-		const part = array.slice(0, array.indexOf(common));
-		if (part[part.length - 1] === mainPathId) part.splice(-1);
+	// Find branches ta start before
 
-		return part;
-	};
-
-	const getStartingSplit = () => {
-		const split = {
-			paths: [] as string[][],
-			mergeAt: "",
-			continueTo: "",
-		};
-		for (let i = 0; i < trips.length; i++) {
-			const currentTrip = trips[i];
-
-			for (let j = 0; j < trips.length; j++) {
-				if (j === i) continue;
-
-				const otherTrip = trips[j];
-
-				const currentIds = currentTrip.map((e) => e.stopId);
-				const otherIds = otherTrip.map((e) => e.stopId);
-				const commonStops = intersection(currentIds, otherIds);
-				const currentPart = extractPathFromSplit(
-					currentIds,
-					commonStops[0]
-				);
-
-				split.paths.push(currentPart);
-				split.mergeAt = commonStops[0];
-				split.continueTo = commonStops[commonStops.length - 1];
-			}
-		}
-
-		return split;
-	};
-
-	const startingSplit = getStartingSplit();
-
-	const path = [startingSplit];
-
-	// Maybe find the "continuesTo" index for each routes, slice the array at this point until the end
-	// and re-apply the same logic for "getStartingSplit"
-
-	return { paths: path, mainPath: paths.mainPath };
+	return [];
 }
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { getPaths } from "./getPaths";
 
 (async () => {
 	const start = Date.now();
-	const timetable = await getTimetable("02-68");
+	const timetable = await getTimetable("01-68");
 	const paths = getPaths(timetable);
 	fs.writeFileSync("./paths.json", JSON.stringify(paths, null, 2));
 	console.log(`Path generated in ${Date.now() - start} ms`);
